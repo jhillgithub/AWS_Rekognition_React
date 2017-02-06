9333f915aeb60da0b785efc1829b0b3f2468b251
jhillgithub <jhillgithub@users.noreply.github.com>
2017-02-06 09:02:38 -0600

    fixed canvas overflow issue

--- webpack.production.config.js
+++ webpack.production.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: "./src/app.js",
   output: {
     path: path.join(__dirname, 'public'),
-    filename: "public/bundle.js",
+    filename: "bundle.js",
     publicPath: '/public/',
     libraryTarget: "umd"
   },
